7007fdd6f5e24cf7ec16050c9900cf58d10b80a2
Jemma Simmons <792036583@qq.com>
2025-04-15 15:19:19 +0800

    Update placestrategy.hh

--- source/algo/placer/placestrategy.hh
+++ source/algo/placer/placestrategy.hh
@@ -2,21 +2,24 @@
 
 #include <std/memory.hh>
 #include <std/collection.hh>
+#include "hardware/interposer.hh"
+#include "circuit/basedie.hh"
 
-namespace kiwi::hardware {
-    class Interposer;
-}
+namespace kiwi::hardware { class Interposer; }
 
-namespace kiwi::circuit {
-    class Net;
-    class TopDieInstance;
-}
+namespace kiwi::circuit { class TopDieInstance; }
 
 namespace kiwi::algo {
 
     struct PlaceStrategy {
+        virtual void configure(
+            const hardware::Interposer& interposer,
+            const circuit::BaseDie& basedie
+        ) = 0;
+
         virtual auto place(
             hardware::Interposer* interposer,
+            circuit::BaseDie* basedie,
             std::Vector<circuit::TopDieInstance>& topdies
         ) const -> void = 0;
     };
