6a940105508a95b25d23e9616930c0d66cfcecb8
Jemma Simmons <792036583@qq.com>
2025-04-15 15:21:25 +0800

    Update saplacestrategy.hh

--- source/algo/placer/sa/saplacestrategy.hh
+++ source/algo/placer/sa/saplacestrategy.hh
@@ -3,31 +3,44 @@
 #include "../placestrategy.hh"
 #include <std/utility.hh>
 #include <std/integer.hh>
+#include <std/collection.hh>
+#include <random>
+#include <cmath>
 
-namespace kiwi::circuit {
-    class TopDieInstance;
-    class Net;
-}
+namespace kiwi::circuit { class TopDieInstance; }
 
 namespace kiwi::algo {
 
     class SAPlaceStrategy : public PlaceStrategy {
     public:
+        SAPlaceStrategy(
+            double initial_temp = 1000.0,
+            double cooling_rate = 0.95,
+            int max_iterations = 10000
+        );
+
+        void configure(
+            const hardware::Interposer& interposer,
+            const circuit::BaseDie& basedie
+        ) override;
+
         virtual auto place(
             hardware::Interposer* interposer,
+            circuit::BaseDie* basedie,
             std::Vector<circuit::TopDieInstance>& topdies
         ) const -> void override;
 
     private:
         auto net_cost(circuit::Net* net) const -> std::i64;
-        auto randomly_choise_one_topdie_insts(std::Vector<circuit::TopDieInstance>& topdies) const -> circuit::TopDieInstance*;
-        auto randomly_choise_two_topdie_insts(std::Vector<circuit::TopDieInstance>& topdies) const -> std::Tuple<circuit::TopDieInstance*, circuit::TopDieInstance*>;
-        auto decide_to_swap_topdie_inst() const -> bool;
+        auto check_placement_legality(const circuit::TopDieInstance* inst) const -> bool;
+        auto randomly_choose_swappable_insts(std::Vector<circuit::TopDieInstance>& topdies) const 
+            -> std::Tuple<circuit::TopDieInstance*, circuit::TopDieInstance*>;
 
-    private:
-        const double _init_temperature {10.0};
-        const double _freeze_temperature {2.0};
-        const std::size_t _solve_number {10};
+        const hardware::Interposer* interposer_ = nullptr;
+        const circuit::BaseDie* basedie_ = nullptr;
+        double initial_temp_;
+        double cooling_rate_;
+        int max_iterations_;
     };
 
 }
