820ef8f7b1e54d3521f5fb6730077948944c8486
Jemma Simmons <792036583@qq.com>
2025-04-15 15:17:04 +0800

    Update place.cc

--- source/algo/placer/place.cc
+++ source/algo/placer/place.cc
@@ -1,14 +1,16 @@
-#include "./place.hh"
-#include "./placestrategy.hh"
+#include "place.hh"
+#include "placestrategy.hh"
 
 namespace kiwi::algo {
 
     auto place(
         hardware::Interposer* interposer,
+        circuit::BaseDie* basedie,
         std::Vector<circuit::TopDieInstance>& topdies,
         const PlaceStrategy& strategy
-    )-> void {
-        strategy.place(interposer, topdies);
+    ) -> void {
+        strategy.configure(*interposer, *basedie); 
+        strategy.place(interposer, basedie, topdies);
     }
 
 }
